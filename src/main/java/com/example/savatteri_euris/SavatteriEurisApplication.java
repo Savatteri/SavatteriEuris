4437265bbb014d5c16bf9af0b88ae39c67d2e774
giuseppe.savatteri <giuseppe.savatteri@selecta.it>
2026-05-18 12:22:33 +0200

    First commit

---
+++ src/main/java/com/example/savatteri_euris/SavatteriEurisApplication.java
@@ -0,0 +1,13 @@
+package com.example.savatteri_euris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavatteriEurisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavatteriEurisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/savatteri_euris/SavatteriEurisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.savatteri_euris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavatteriEurisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
